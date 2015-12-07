5ab76400d867858805201861e0fc8bc48711e0d9
jeffolen4 <jmo@olen-inc.com>
2015-12-06 18:04:47 -0800

    fixed add link to format correctly if adding to a table row

--- vendor/assets/javascripts/jquery_nested_form.js
+++ vendor/assets/javascripts/jquery_nested_form.js
@@ -59,23 +59,28 @@
       return new Date().getTime();
     },
     insertFields: function(content, assoc, link) {
-      var target = $(link).data('target');
-      if (target) {
-        return $(content).appendTo($(target));
+      var $tr = $(link).closest('tr');
+      if ($tr.length > 0) {
+        return $(content).insertBefore($tr);
       } else {
-        return $(content).insertBefore(link);
+        var target = $(link).data('target');
+        if (target) {
+          return $(content).appendTo($(target));
+        } else {
+          return $(content).insertBefore(link);
+        }
       }
-    },
+    },    
     removeFields: function(e) {
       var $link = $(e.currentTarget),
           assoc = $link.data('association'); // Name of child to be removed
-      
+
       var hiddenField = $link.prev('input[type=hidden]');
       hiddenField.val('1');
-      
+
       var field = $link.closest('.fields');
       field.hide();
-      
+
       field
         .trigger({ type: 'nested:fieldRemoved', field: field })
         .trigger({ type: 'nested:fieldRemoved:' + assoc, field: field });
